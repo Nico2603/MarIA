5f9315b55cdcef72b55a398244ae5d578c73b79c
Nico2603 <nicolasceballosbrito@gmail.com>
2025-05-03 11:12:12 -0500

    Fixeo de Auth en Render

---
+++ src/lib/auth.ts
@@ -0,0 +1,29 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
+export const authOptions: AuthOptions = {
+  // @ts-ignore // Ignora temporalmente posible problema de tipos con Adapter
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  session: {
+    strategy: "database",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  // Puedes añadir páginas personalizadas si quieres
+  // pages: { ... }
+}; 
