b5c7435ddeda9bd5ffb36a2de76e00489bfb36d7
Nico2603 <nicolasceballosbrito@gmail.com>
2025-06-15 16:19:56 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter, Montserrat } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import Providers from '@/components/Providers'
 import AuthProvider from '@/components/providers/SessionProvider'
@@ -35,8 +36,34 @@ export default function RootLayout({
     <html lang="es" className={`${inter.variable} ${montserrat.variable} h-full`}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N9C77NVD');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className={`flex flex-col min-h-screen ${inter.className} ${montserrat.className}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N9C77NVD"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <AuthProvider>
           <Providers>
             <Header />
